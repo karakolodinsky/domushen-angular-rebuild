75a4f2e86c9b581249e04ac09ebaf5eb5e8abde3
kara_kolodinsky <ktk4111@rit.edu>
2023-11-02 19:04:08 -0400

    MORE IMAGES added to the THING

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   }
 
   swap() {
-    const numimages = 31;
+    const numimages = 40;
     const rndimg = [
       "../assets/header/0.png", "../assets/header/1.png", "../assets/header/2.png", "../assets/header/3.png", "../assets/header/4.png",
       "../assets/header/5.png", "../assets/header/6.png", "../assets/header/7.png", "../assets/header/8.jpg", "../assets/header/9.jpg",
@@ -22,7 +22,8 @@ export class HeaderComponent implements OnInit {
       "../assets/header/15.jpg", "../assets/header/16.gif", "../assets/header/17.gif", "../assets/header/18.png", "../assets/header/19.jpg",
       "../assets/header/20.png", "../assets/header/21.png", "../assets/header/22.png", "../assets/header/23.png", "../assets/header/24.jpg",
       "../assets/header/25.png", "../assets/header/26.png", "../assets/header/27.jpg", "../assets/header/28.gif", "../assets/header/29.gif",
-      "../assets/header/30.jpg"
+      "../assets/header/30.jpg", "../assets/header/31.png", "../assets/header/32.png", "../assets/header/33.png","../assets/header/34.JPG", 
+      "../assets/header/35.jfif", "../assets/header/36.JPG", "../assets/header/37.gif", "../assets/header/38.gif", "../assets/header/40.PNG"
     ];
 
     const x = Math.floor(Math.random() * numimages);
